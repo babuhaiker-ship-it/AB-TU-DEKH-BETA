f6dd2019322a6eb73feb7fcb6ad108692ddf75a5
Godfatherpy <fschool749@gmail.com>
2025-06-23 22:13:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 
 # --- Configuration ---
 class BotConfig:
-    BOT_TOKEN = '7946785578:AAGzx5tFfZyIT6LxtpEQnkX9EA1TdqSgkCs' # Replaced with a dummy token for safety
+    BOT_TOKEN = '7213744072:AAFK7siFferpBbfqokmPu4l_5fK6ytzhpiM' # Replaced with a dummy token for safety
     API_ID = 29800015
     API_HASH = 'c8f37108be31ab9ea2818bfe533fbb6f'
     BOT_USERNAME = '@Spicynyraabot'
