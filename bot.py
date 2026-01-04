427f63fc0a8bd34dc41efcdc451f0522d994881d
Godfatherpy <fschool749@gmail.com>
2026-01-04 13:55:14 +0530

    Update host URL and modify bot URL

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class BotConfig:
     BOT_USERNAME = '@SpicyNyraa_bot'
     MONGO_URI = 'mongodb+srv://Pyasipriya:00pEcao9sYhNC5VQ@cluster0.2dfenf7.mongodb.net/spicybot?retryWrites=true&w=majority&appName=Cluster0'
     MONGO_DB_NAME = 'spicybot'
-    HOST = "http://localhost:8000"  # Replace with your actual host URL
+    HOST = "http://38.109.11.123"  # Replace with your actual host URL
     # REMOVED: VIDEO_CHANNEL_ID = -1002621716446 # Your video storage channel ID
     BUY_BOT_URL = 'https://t.me/SpicyNyraaSupport_bot' # MODIFIED: Added https://
     OWNER_ID = 6612030110 # The main owner ID, cannot be removed
